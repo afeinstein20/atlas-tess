faf7685dce5c65f51566da7998fbe33bdb316dbb
Adina Feinstein <adina.d.feinstein@gmail.com>
2025-08-06 08:59:58 -0400

    new figure 3 script

--- scripts/figure7.py
+++ scripts/figure7.py
@@ -19,15 +19,18 @@ second= np.load('../data/stacked_3I_1-2_v4.npy', allow_pickle=True).item()
 
 # Camera 2 CCD 3 data
 time1 = first['time'] + 2400000.5 - 2457000
-q = time1 > 3804
+q = (time1 > 3804) & (first['good_frames'] == 0)
 time1 = time1[q]
 lc1 = first['subtracted'][:,9,9][q]
 bkg1 = first['subtracted'][:,7,12][q]
 
 # Camera 1 CCD 2 data
 time2 = second['time'] + 2400000.5 - 2457000
-lc2 = second['subtracted'][:,9,9]
-bkg2 = second['subtracted'][:,7,12]
+q = (second['good_frames'] == 0) & (time2 > 3818) & (time2 < 3828)
+
+time2 = time2[q]
+lc2 = second['subtracted'][:,9,9][q]
+bkg2 = second['subtracted'][:,7,12][q]
 
 # Periodogram function
 def periodogram(t, f, maxp=1*units.hour, minp=3*units.day):
@@ -46,8 +49,10 @@ axes = axes.reshape(-1)
 dc = '#de4f0d'
 bc = '#ffbe35'
 
+q = (first['good_frames'] == 0)
+
 # Imshow the TPF
-axes[0].imshow(np.nansum(first['subtracted'], axis=0), aspect='auto', cmap='Greys_r')
+axes[0].imshow(np.nanmedian(first['subtracted'][q], axis=0), aspect='auto', cmap='Greys_r')
 
 # Plot the apertures
 rect = Rectangle((8.25, 8.25), 1.5, 1.5, edgecolor=dc, facecolor='none', lw=3)
@@ -68,11 +73,10 @@ axes[1].plot((1.0/ls_23[0]).to(units.hour), ls_23[1], color=dc, lw=2)
 # Camera 1 CCD 2 #
 ##################
 
-q = (time2 >= 3818) & (time2 < 3827.8) #& (lc2 < 20.) & (bkg2 > 19.5)
-
+q = second['good_frames'] == 0 #& (lc2 < 20.) & (bkg2 > 19.5)
 
 # Plot the TPF
-axes[2].imshow(np.nansum(second['subtracted'][second['good_frames']==0], axis=0),
+axes[2].imshow(np.nanmedian(second['subtracted'][q], axis=0),
                aspect='auto',
                cmap='Greys_r')
 
@@ -85,10 +89,10 @@ axes[2].add_patch(rect)
 
 
 # Make the Lomb-Scargle Periorograms
-bkg_12, mb12 = periodogram(time2[q], bkg2[q])
+bkg_12, mb12 = periodogram(time2, bkg2)
 axes[3].plot((1.0/bkg_12[0]).to(units.hour), bkg_12[1], color=bc)
 
-ls_12, m12 = periodogram(time2[q], lc2[q])
+ls_12, m12 = periodogram(time2, lc2)
 axes[3].plot((1.0/ls_12[0]).to(units.hour), ls_12[1], color=dc, lw=2)
 
 
@@ -97,11 +101,18 @@ axes[3].set_xlabel('Period [hours]')
 for i in [1,3]:
     axes[i].set_xlim(1,70)
     axes[i].set_ylabel('Power')
+    if i == 1:
+        label = '16.16 hours'
+    else:
+        label=''
+    axes[i].axvline(16.16, color='k', linestyle=':', label=label)
+
+axes[1].legend()
 
 for i in [0,2]:
     axes[i].set_xticks([])
     axes[i].set_yticks([])
-axes[3].set_ylim(0,0.04)
+axes[3].set_ylim(0,0.06)
 axes[1].set_ylim(0,0.04)
 axes[1].set_xticklabels([])
 
